e39d016db00587c1e9c49a5e6939a77d5d1f6818
Kate Schlunz <schlunzk@gmail.com>
2018-02-13 16:08:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,8 +9,19 @@ function breakOut(array, changeValue, stopValue){
          break;
 
         }
-array[i]=changeValue;
+        array[i]=changeValue;
 
     }
 return array;
+}
+
+
+function keepGoing(array, changeValue, skipValue){
+  for (let i =0; i<array.length; i++){
+    if (array[i]=== skipValue){
+      continue
+    }
+    array[i] = changeValue;
+  }
+  return array; 
 }
